07f8d55534aef9a96ee307e091c4780e28967655
Noor Ncho <nrandin@gmail.com>
2021-08-16 16:13:54 +0100

    Set up eventListner for buttons

--- scripts/dist/script.dev.js
+++ scripts/dist/script.dev.js
@@ -2,3 +2,29 @@
 
 var CALCULATOR = document.querySelector(".calculator");
 var KEYS = CALCULATOR.querySelector(".calculator__keys");
+KEYS.addEventListener("click", function (e) {
+  if (e.target.matches("button")) {
+    var key = e.target;
+    var action = key.dataset.action;
+
+    if (!action) {
+      console.log("number key!");
+    }
+
+    if (action === "add" || action === "subtract" || action === "divide" || action === "multiply") {
+      console.log("operator key");
+    }
+
+    if (action === "clear") {
+      console.log("Clear key");
+    }
+
+    if (action === "decimal") {
+      console.log("decimal key");
+    }
+
+    if (action === "solve") {
+      console.log("equals key");
+    }
+  }
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,30 @@
 const CALCULATOR = document.querySelector(".calculator");
 const KEYS = CALCULATOR.querySelector(".calculator__keys");
+
+KEYS.addEventListener("click", e =>{
+
+    if(e.target.matches("button")){
+        const key = e.target;
+        const action = key.dataset.action;
+        
+        if(!action){
+            console.log("number key!");
+        }
+
+        if(action === "add" || action === "subtract" || action === "divide" || action === "multiply"){
+            console.log("operator key");
+        }
+
+        if(action === "clear"){
+            console.log("Clear key");
+        }
+
+        if(action === "decimal"){
+            console.log("decimal key");
+        }
+
+        if(action === "solve"){
+            console.log("equals key")
+        }
+    }
+});
